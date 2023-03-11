6b962687e9a351dd71628e5c44f03e923512b322
guchi5 <zero19390901@gmail.com>
2023-03-11 18:12:03 +0900

    1st commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,8 +13,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
+	@RequestMapping("/sample")
 	String sayHello() {
-		return "Hello World!";
+		return "Hello";
 	}
 }
